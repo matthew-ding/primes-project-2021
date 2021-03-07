141f73872f1fe10f9d66237a72a6bb42d63f4405
Matthew Ding <matthew.c.ding2012@gmail.com>
2021-03-07 11:02:08 -0500

    fixed node initialization

--- relayCompare.py
+++ relayCompare.py
@@ -1,5 +1,5 @@
+import copy
 import random
-
 import RelayConsensus
 from graphGenerator import get_relay_graph
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ accuracy = 0.03
 iters = 1
 
 stdList = []
-itersList = []
+freqList = []
 
 # Graph generation
 adjList, byzantine_set, m, diameter = get_relay_graph(bsize)
@@ -20,24 +20,26 @@ print("Graph Generated Successfully")
 # Node initialization
 nodeList = []  # list of all nodes
 for i in range(m):
-    # parameter arrays
     nodeList.append(RelayConsensus.Node(i, m, 1.0 * random.randrange(-110, 110)))
 
-for i in range(1, 3*bsize):
+
+for i in range(1, 2 * bsize + 5):
+    tempNodeList = copy.deepcopy(nodeList)
     updateFreq = i
-    print("Update Frequency: " + str(updateFreq))
+
     # driver function
-    finalIters, finalNodeList, m, byzantine_set, std = RelayConsensus.IABC(iters, maxIter, updateFreq, accuracy, adjList, byzantine_set, m, diameter, nodeList)
-    stdList.append(updateFreq)
-    itersList.append(finalIters)
+    finalIters, finalNodeList, m, byzantine_set, std = RelayConsensus.IABC(iters, maxIter, updateFreq, accuracy,
+                                                                           adjList, byzantine_set, m, diameter,
+                                                                           tempNodeList)
+    freqList.append(updateFreq)
+    stdList.append(std)
 
+    print("Update Frequency: " + str(updateFreq))
+    print("Standard Deviation: " + str(std))
 
-plt.plot(stdList, itersList)
+plt.plot(freqList, stdList)
 plt.title('Relay Speed vs. Update Frequency')
-plt.xlabel('Iterations per Update')
+plt.xlabel("Update Frequency")
 plt.ylabel('Standard Deviation')
+plt.yscale("log")
 plt.show()
-
-
-
-
