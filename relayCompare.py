e53994c55d5568f237e5b5da190a96fca68659d2
Matthew Ding <matthew.c.ding2012@gmail.com>
2021-03-18 23:44:53 -0400

    added repetitive simulations

--- relayCompare.py
+++ relayCompare.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # hyperparameters
 hsize = 100
 bsize = 15
-maxIter = 300
+maxIter = 1000
 accuracy = 0.03
 iters = 1
 
@@ -15,34 +15,41 @@ stdList = []
 freqList = []
 
 # Graph generation
-adjList, byzantine_set, m, diameter, density = graphGenerator.get_random_graph(200)
+adjList, byzantine_set, m, diameter, density = graphGenerator.get_linear_graph(50)
 print("Graph Generated Successfully")
 print("Size: " + str(m))
 print("Diameter: " + str(diameter))
 print("Density: " + str(density))
 
 # Node initialization
-nodeList = []  # list of all nodes
+nodeList = [RelayConsensus.Node(0, 0, 0)] * m  # list of all nodes
+valueList = []  # list of all initial node values
+# creating list of initial values
 for i in range(m):
-    nodeList.append(RelayConsensus.Node(i, m, 1.0 * random.randrange(-110, 110)))
+    valueList.append(1.0 * random.randrange(-110, 110))
 
 
-for i in range(1, diameter+1):
-    print(" ")
+for i in range(1, diameter + 1):
+    freqList.append(i)
+    tempStdList = []
+    for repetitions in range(5):
+        for j in range(m):
+            nodeList[j] = RelayConsensus.Node(j, m, valueList[j])
+        print(" ")
+        print("Update Frequency: " + str(i))
+        print(" ")
+        tempNodeList = copy.deepcopy(nodeList)
+        updateFreq = i
+
+        # driver function
+        finalIters, finalNodeList, m, byzantine_set, std = RelayConsensus.IABC(iters, maxIter, updateFreq, accuracy,
+                                                                               adjList, byzantine_set, m, diameter,
+                                                                               tempNodeList)
+
+        tempStdList.append(std)
+    stdList.append(sum(tempStdList) / len(tempStdList))
     print("Update Frequency: " + str(i))
-    print(" ")
-    tempNodeList = copy.deepcopy(nodeList)
-    updateFreq = i
-
-    # driver function
-    finalIters, finalNodeList, m, byzantine_set, std = RelayConsensus.IABC(iters, maxIter, updateFreq, accuracy,
-                                                                           adjList, byzantine_set, m, diameter,
-                                                                           tempNodeList)
-    freqList.append(updateFreq)
-    stdList.append(std)
-
-    print("Update Frequency: " + str(updateFreq))
-    print("Standard Deviation: " + str(std))
+    print("Standard Deviation: " + str(sum(tempStdList) / len(tempStdList)))
 
 plt.plot(freqList, stdList)
 plt.title("Nodes: " + str(m) + ", " + "Diameter: " + str(diameter) + ", Density: " + str(density))
